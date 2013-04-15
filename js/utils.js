5d7b44910cb5ebcc57b2062877fa24ad6a90796f
Rahul <rahul@digitalgreen.org>
2013-04-15 11:45:47 -0400

    Integrating the new data files

--- js/utils.js
+++ js/utils.js
@@ -3,50 +3,46 @@
 
 Returns a javascript array of expenses. A sample expense looks like - 
     {
-      "id": 109,
-      "description": "Walmart Commons",
-      "amount": 20.68,
-      "category": null,   (null because none assigned right now)
-      "date": "2013-04-10T04:00:00.000Z",  (javascript data object)
-      "user_data": [    (array of expense_user entries for this expense)
+      "eid": 1691,
+      "description": "Getty",
+      "amount": 59,
+      "category": "transportation",
+      "date": "2013-09-18T04:00:00.000Z",
+      "user_data": [
         {
-          "id": 109,
-          "user_id": 1,
-          "paid": 20.68,
-          "share": 6.89
+          "eid": 1691,
+          "uid": 2,
+          "paid": 41.3,
+          "share": 59
         },
         {
-          "id": 109,
-          "user_id": 2,
-          "paid": 0,
-          "share": 6.9
-        },
-        {
-          "id": 109,
-          "user_id": 3,
-          "paid": 0,
-          "share": 6.89
+          "eid": 1691,
+          "uid": 1,
+          "paid": 17.7,
+          "share": 59
         }
       ]
     }
+
 */
 function getData(callBack) {
-    var format = d3.time.format("%Y-%m-%d");
+    var format_old = d3.time.format("%Y-%m-%d");
+    var format = d3.time.format("%m/%d/%Y");
     d3.tsv('data/expense_table.txt')
-        .row(function(d) { return {id: +d.id, description: d.description, 
+        .row(function(d) { return {eid: +d.eid, description: d.description, 
                                    amount: +d.amount, category: d.category=="None"?null:d.category, 
                                    date: format.parse(d.date)}; 
                          })
         .get(function(error, exp_rows) { 
             d3.tsv('data/expense_user_table.txt')
             .row(function(d) {
-                return {id:+d.id, user_id:+d.user_id, paid:+d.paid, share:+d.share};
+                return {eid:+d.eid, uid:+d.uid, paid:+d.paid, share:+d.share};
             })
             .get(function(error, exp_user_rows) {
                  var exp_user_map = d3.nest()
-                    .key(function(d) { return d.id; })
+                    .key(function(d) { return d.eid; })
                     .map(exp_user_rows, d3.map);
-                 var combined_data = exp_rows.map(function(row) {row.user_data = exp_user_map.get(row.id); return row;})
+                 var combined_data = exp_rows.map(function(row) {row.user_data = exp_user_map.get(row.eid); return row;})
                  callBack(combined_data); 
             });
         });
